7146cd05be8e4a9e27ecf02de1b108462961c53d
Carrie Song <carrie.c.song@gmail.com>
2018-10-22 10:18:53 -0700

    Updated Oct 21

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+// var char1 = $("<img>");
+// $(char1).attr("src","assets/images/pikachu.png");
+// $("#characterArea").append(char1);
+$("#img-div1").html("<img src='assets/images/pikachu.png' class='char1'width = 100px height = 100px />");
+
